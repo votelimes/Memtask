924aaba4ab5a084391b074f393fbfa81c6f98957
mcw1999 <lookslike8@outlook.com>
2021-01-26 00:44:17 +0300

    0.006a

--- app/src/main/java/com/example/clock/MainActivity.java
+++ app/src/main/java/com/example/clock/MainActivity.java
@@ -2,42 +2,38 @@ package com.example.clock;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.Shape;
 import android.os.Bundle;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
-import android.util.TypedValue;
-import android.view.MotionEvent;
 import android.view.View;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
-import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomSheetBehavior mBottomSheetBehavior;
     private List<AlarmNote> alarmNoteList;
     private AlarmNoteDao database;
-    private String[] dayNames = {"NULL", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+    private final String[] dayNames = {"NULL", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
     private LinearLayout userNoteLayout;
     private Context mContext;
     private int lastClickedUserNoteIndex = -1;
+    Calendar chosen_time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,19 +59,20 @@ public class MainActivity extends AppCompatActivity {
             addNoteToLayout(element);
         }
 
-        //debugTestDB();
+        chosen_time = Calendar.getInstance();
     }
 
     public void onAddClock(View view) {
         Intent clock_window = new Intent(this, TimePickerFull.class);
         AlarmNote selectedNote = null;
-        for(AlarmNote note : alarmNoteList){
-            if(note.alarmNoteId == lastClickedUserNoteIndex){
-                selectedNote = note;
-                break;
+        if(lastClickedUserNoteIndex != -1) {
+            for (AlarmNote note : alarmNoteList) {
+                if (note.alarmNoteId == lastClickedUserNoteIndex) {
+                    selectedNote = note;
+                    break;
+                }
             }
         }
-
         clock_window.putExtra("selectedNote", selectedNote);
         startActivityForResult(clock_window, 1);
     }
@@ -93,7 +90,13 @@ public class MainActivity extends AppCompatActivity {
         params.setMargins(0, 30, 0, 30);
 
         newNoteLayout.setLayoutParams(params);
+        TextView time = (TextView) newNoteLayout.findViewWithTag("time_day_of_week")
+                .findViewWithTag("time");
+        time.setText(timeInMillisToTime(note.getTimeInMillis()));
 
+        TextView day_of_week = (TextView) newNoteLayout.findViewWithTag("time_day_of_week")
+                .findViewWithTag("day_of_week");
+        day_of_week.setText(dayNames[note.getDayOfWeek()]);
 
         userNoteLayout.addView(newNoteLayout);
 
@@ -101,18 +104,26 @@ public class MainActivity extends AppCompatActivity {
         newNoteLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    changeStrokeColor(findViewById(lastClickedUserNoteIndex), getColor(R.color.light_green));
-                } catch (Exception e){
-                    e.printStackTrace();
+                if(lastClickedUserNoteIndex == v.getId()){
+                    try {
+                        changeStrokeColor(findViewById(lastClickedUserNoteIndex), getColor(R.color.light_green));
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    lastClickedUserNoteIndex = -1;
+                } else{
+                    try {
+                        changeStrokeColor(findViewById(lastClickedUserNoteIndex), getColor(R.color.light_green));
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    try{
+                        changeStrokeColor(v, getColor(R.color.red));
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    lastClickedUserNoteIndex = v.getId();
                 }
-                try{
-                    changeStrokeColor(v, getColor(R.color.red));
-                } catch (Exception e){
-                    e.printStackTrace();
-                }
-
-                lastClickedUserNoteIndex = v.getId();
             }
         });
     }
@@ -125,8 +136,8 @@ public class MainActivity extends AppCompatActivity {
     public void onNoteClick(View view){
 
     }
-    private void printCloseNotes(long timeInMillis){
-        clearNoteLayout();
+    private void printCloseNotes(){
+        long timeInMillis = chosen_time.getTimeInMillis();
         for(AlarmNote note : alarmNoteList){
             long closeTimeBarrier = note.getTimeInMillis() - timeInMillis;
             if(closeTimeBarrier <= AlarmNote.DAY){
@@ -141,34 +152,47 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult (int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
+        boolean  success = false;
         if (resultCode == 1){
             AlarmNote new_note = (AlarmNote) data.getSerializableExtra("result");
             try{
                 database.insert(new_note);
                 alarmNoteList.add(new_note);
+                success = true;
             } catch(Exception e){
                 e.printStackTrace();
             }
         }
-        if(resultCode == 2){
+        else if(resultCode == 2){
             AlarmNote new_note = (AlarmNote) data.getSerializableExtra("result");
             try{
                 database.update(new_note);
                 for(int i = 0; i < alarmNoteList.size(); i++){
                     if(alarmNoteList.get(i).alarmNoteId == new_note.alarmNoteId){
                         alarmNoteList.set(i, new_note);
+                        success = true;
                         break;
+
                     }
                 }
             } catch (Exception e){
                 e.printStackTrace();
             }
         }
+        if(success){
+            clearNoteLayout();
+            printCloseNotes();
+        }
     }
 
     // Utility methods
     public int pxToDp(Context context, int px) {
         return  ((int) (px / context.getResources().getDisplayMetrics().density));
+    }
+    public String timeInMillisToTime(long timeInMillis){
+        Date time = new Date(timeInMillis);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        return sdf.format(time);
     }
 
     // Debug methods, have to be removed before release
@@ -196,5 +220,4 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d("DB_TESTING", "CUSTOM_TEST_END");
     }
-
 }
